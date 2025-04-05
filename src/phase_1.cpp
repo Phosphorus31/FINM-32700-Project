cd9f6c8a6afd256dbff7f4cf5af918ccb683ccd5
Kevin Wang <kkevinwang2003@gmail.com>
2025-04-05 16:59:50 -0500

    Phase 1 part 1 member 2

--- src/phase_1.cpp
+++ src/phase_1.cpp
@@ -14,3 +14,18 @@ void multiply_mv_row_major(const double* matrix, int rows, int cols,
         result[i] = sum;
     }
 }
+
+void multiply_mv_col_major(const double* matrix, int rows, int cols,
+                           const double* vector, double* result) {
+    // TODO: error handling
+
+    for (int i = 0; i < rows; ++i) {
+        result[i] = 0.0;
+    }
+
+    for (int j = 0; j < cols; ++j) {
+        for (int i = 0; i < rows; ++i) {
+            result[i] += matrix[j * rows + i] * vector[j];
+        }
+    }
+}
--- tests/test.cpp
+++ tests/test.cpp
@@ -15,9 +15,26 @@ int main() {
 
     if (result[0] == 14.0 && result[1] == 32.0) {
         std::cout << "Test passed!" << std::endl;
-        return 0;
+        // return 0;
     } else {
         std::cout << "Test failed!" << std::endl;
-        return 1;
+        // return 1;
     }
+
+    // column-major storage
+    // [1 2 3]
+    // [4 5 6]
+    double matrix_2[] = {1.0, 4.0, 2.0, 5.0, 3.0, 6.0};
+    
+    multiply_mv_col_major(matrix_2, rows, cols, vector, result);
+    if (result[0] == 14.0 && result[1] == 32.0) {
+        std::cout << "Test passed!" << std::endl;
+        // return 0;
+    } else {
+        std::cout << "Test failed!" << std::endl;
+        // return 1;
+    }
+
+    return 0;
+
 }
